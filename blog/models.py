3410aedf9be2f29f9f00c50ef1a5e0dba3054b9a
Charon <Charon@Charon.local>
2018-06-04 22:14:44 +0800

    excerpt and db

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
+import markdown
+from django.utils.html import strip_tags
 # Create your models here.
 
 class Category(models.Model):
@@ -45,3 +46,18 @@ class Post(models.Model):
         self.views += 1
         self.save(update_fields=['views'])
 
+    def save(self, *args, **kwargs):
+        # 如果没有填写摘要
+        if not self.excerpt:
+            # 首先实例化一个Markdown类, 用于渲染body的文本
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+            ])
+            # 先将 Markdown 文本渲染成 HTML 文本
+            # strip_tags 去掉 HTML 文本的全部 HTML 标签
+            # 从文本摘取前 54 个字符赋给 excerpt
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+
+        # 调用父类的 save 方法将数据保存到数据库中
+        super(Post, self).save(*args, **kwargs)
